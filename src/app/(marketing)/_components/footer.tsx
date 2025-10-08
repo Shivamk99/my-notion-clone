813b39e9f37ab8df6b60b0fdd56cbcd1a9a3ee62
Shivamk99 <smileyshivam3042@gmail.com>
2025-10-08 15:13:28 +0530

    init: project with pkgs

---
+++ src/app/(marketing)/_components/footer.tsx
@@ -0,0 +1,11 @@
+import Logo from "./logo";
+
+export const Footer = () => {
+  return (
+    <div className={"flex items-cneter w-full p-6 bg-background z-50"}>
+      <Logo />
+    </div>
+  );
+};
+
+export default Footer;
